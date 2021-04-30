4c69fb9ef90b7c0830ccc126cc7e8bc054c0114e
Piotr Kiczkajło <kiczkajlo.p@gmail.com>
2021-04-30 22:00:15 +0200

    add function to script

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
-console.log("Czesc wszystkim");
-
-let changeButton = document.querySelector(".js-passion__button");
-let pictureGeisza = document.querySelector(".js-passion__picGeisza");
-let pictureBalerina = document.querySelector(".js-passion__picBalerina");
-let pictureFlamenco = document.querySelector(".js-passion__picFlamenco");
-
-changeButton.addEventListener("click", () => {
-    if (changeButton.innerText === "Pokaż obraz") {
-        changeButton.innerText = "Zmień obraz";
-        pictureGeisza.classList.toggle("showPicture");
-    } else if (pictureGeisza.classList.contains("showPicture")) {
-        pictureGeisza.classList.toggle("showPicture");
-        pictureBalerina.classList.toggle("showPicture");
-    } else if (pictureBalerina.classList.contains("showPicture")) {
-        pictureBalerina.classList.toggle("showPicture");
-        pictureFlamenco.classList.toggle("showPicture");
-    } else {
-        pictureFlamenco.classList.toggle("showPicture");
-        pictureGeisza.classList.toggle("showPicture")
+{
+    const welcome = () => {
+        console.log("Czesc wszystkim");
     }
-});
+    const showPicture = () => {
+        const pictureGeisza = document.querySelector(".js-passion__picGeisza");
+        const pictureBalerina = document.querySelector(".js-passion__picBalerina");
+        const pictureFlamenco = document.querySelector(".js-passion__picFlamenco");
+        const changeButton = document.querySelector(".js-passion__button");
+        if (changeButton.innerText === "Pokaż obraz") {
+            changeButton.innerText = "Zmień obraz";
+            pictureGeisza.classList.toggle("showPicture");
+        } else if (pictureGeisza.classList.contains("showPicture")) {
+            pictureGeisza.classList.toggle("showPicture");
+            pictureBalerina.classList.toggle("showPicture");
+        } else if (pictureBalerina.classList.contains("showPicture")) {
+            pictureBalerina.classList.toggle("showPicture");
+            pictureFlamenco.classList.toggle("showPicture");
+        } else {
+            pictureFlamenco.classList.toggle("showPicture");
+            pictureGeisza.classList.toggle("showPicture");
+        }
+    }
+    const init = () => {
+        const changeButton = document.querySelector(".js-passion__button");
+        changeButton.addEventListener("click", showPicture);
+        welcome();
+    }
+    init();
+}
